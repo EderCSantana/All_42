c6394c5327c9e124e509277d95fcfa6a0809a278
EderCSantana <eder.cardoso.santana@gmail.com>
2023-08-14 23:53:57 +0200

    hehr
    passing the docs to piscine folder

--- shell01/ex01/print_groups.sh
+++
@@ -1,2 +0,0 @@
-id -ng $FT_USER | tr '' ',' | tr -d '\n'
-
--- shell01/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-cat /etc/passwd | grep -v "#" | sed -n 'n;p' | cut -f 1 -d : | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ',' | sed 's:,:, :g' | sed 's:, $:.:' | tr -d '\n'
-#cat /etc/passwd | sed '/^#/d' | sed -n '2~2p' | cut -d: -f1 | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ', ' | sed 's/, $/./'
