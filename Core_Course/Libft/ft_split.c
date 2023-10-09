982379522df082957070f97d23e623903205a021
EderCSantana <eder.cardoso.santana@gmail.com>
2023-10-09 16:19:05 +0200

    s

--- Core_Course/Libft/ft_split.c
+++ Core_Course/Libft/ft_split.c
@@ -13,6 +13,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_substr(char const *s, unsigned int start,size_t len)
+{
+	size_t	i;
+	char	*result;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	else if (start > ft_strlen(s))
+		return (NULL);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+        return (NULL);
+	while (s[start + i] != '\0' && i < len)
+	{
+		result[i] = s[start + i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+static size_t	ft_cut_number(const char *s, char c)
+{
+	int	i;
+	size_t	count;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			count ++;
+	}
+	return (count);
+}
+
+static size_t ft_strs(const char *s,char c)
+{
+	int	i;
+	size_t	count;
+
+	i = 0;
+	while (s[i] != '\0' && s[i] == c)
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
 /**
  * @brief Allocates (with malloc(3)) and returns an array
 of strings obtained by splitting ’s’ using the
@@ -27,36 +90,65 @@ with a NULL pointer.
 char **ft_split(char const *s, char c)
 {
 	int	i;
-	int	j;
-	int	count;
 	char** matrix;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			count++;
-		i++;
-	}
-	matrix = malloc(count * sizeof(char *));
+	if (!s)
+		return (NULL);
+	matrix = malloc((ft_cut_number(s, c) + 1) * sizeof(char *));
+	if (!matrix)
+		return (NULL);
 	i = 0;
-	while (i < count)
-	{
-		matrix[i] = malloc(i * sizeof(char));
-		i++;
-	}
-
-	count = 0;
 	while (s[i] != '\0')
 	{
-		j = 0;
-		while (s[j] != c)
+		if (s[i] != c)
 		{
-			matrix[i][j] = s[i];
-			j++;
-		}
-		if (s[i] == c)
+			matrix[i] = ft_substr(s, i - ft_strs(s, c), ft_strs(s,c));
+			if (!matrix[i])
+				return (0);
 			i++;
-		i++;
+		}
+		else
+			i++;
+	}
+	matrix[i] = '\0';
+	return matrix;
+}
+
+
+void imprimir_matriz_char(char **matriz)
+{
+	int linhas = 0;
+	int colunas = 0;
+
+  // Encontre o número de linhas e colunas da matriz
+	for (int i = 0; matriz[i] != NULL; i++) {
+    linhas++;
+    colunas = ft_strlen(matriz[i]);
 	}
 
+  // Imprima a matriz
+	for (int i = 0; i < linhas; i++) {
+    for (int j = 0; j < colunas; j++) {
+	printf("%c ", matriz[i][j]);
+    }
+    printf("\n");
+	}
+}
+int main()
+{
+	char* string = "chupa que a cana eh doce meu filho";
+	char remove = ' ';
+	char** matriz;
+
+	matriz = ft_split(string, remove);
+	if (matriz == NULL) {
+		printf("Erro ao alocar memória para a matriz de strings\n");
+		return 1;
+	}
+	imprimir_matriz_char(matriz);
+	for (int i = 0; matriz[i] != NULL; i++) {
+		free(matriz[i]);
+	}
+	free(matriz);
+	return 0;
 }
