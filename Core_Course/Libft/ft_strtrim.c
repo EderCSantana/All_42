bb5d85cd558a09d26d0f77f61dbbe2ba1f1f5bc3
EderCSantana <eder.cardoso.santana@gmail.com>
2023-10-26 17:12:56 +0200

    brabo

--- Core_Course/Libft/ft_strtrim.c
+++ Core_Course/Libft/ft_strtrim.c
@@ -10,81 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-// #include <libft.h>
-
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[0] == '\0')
-		return (0);
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-int	ft_char_counter(char const *s1, char const c)
-{
-	int	count;
-	int	i;
-
-	count = 0;
-	i = 0;
-	while (s1[i] < ft_strlen(s1))
-	{
-		if (s1[i] == c)
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-int	ft_isin(char const *set, char const c)
-{
-	int	i;
-
-	i = 0;
-	while (i < ft_strlen(set))
-	{
-		if (set[i] == c)
-			return (1);
-	}
-	return (0);
-}
-
-char const	*ft_slack(char const *s1, char *s2, char *set, int i)
-{
-	int		last;
-	int		start;
-
-	while (i > 0)
-	{
-		if (ft_isin(set, s1[i]))
-			i--;
-		else
-		{
-			last = i;
-			i = 0;
-		}
-	}
-	while (i < last)
-		i++;
-	start = i;
-	i = 0;
-	while (i < last)
-	{
-		s2[i] = s1[i + start];
-		i++;
-	}
-	s2[i] = '\0';
-	return (s2);
-}
-
 /**
  * @brief Allocates (with malloc(3)) and returns a copy of
 ’s1’ with the characters specified in ’set’ removed
@@ -96,30 +21,18 @@ from the beginning and the end of the string.
 NULL if the allocation fails
  */
 char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	int		i;
-// 	char	*s2;
-
-// 	i = ft_strlen(s1);
-// 	if (!s1 || !set)
-// 		return (NULL);
-// 	s2 = (char *) malloc((ft_strlen(s1) + 1) * sizeof(char));
-// 	if (!s2)
-// 		return (NULL);
-// 	s2 = ft_slack(s1, s2, set, i);
-// 	return (*s2);
-// }
 {
-	int	i;
-	char *s2;
+	size_t last;
 
 	if(!s1)
 		return (NULL);
-	while(*s1 && ft_strchr)
+	while(*s1 != '\0' && ft_strchr(set, s1*))
 		s1++;
+	last = ft_strlen(s1);
+	while (last && ft_strchr(set, s1[last]))
+		last--;
+	return (ft_substr(s1, 0, last + 1));
 }
-
-
 
 // int main()
 // {
